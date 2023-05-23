a076edaa9c68c1d0b7e297ffda55cb520ec7a25e
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-23 15:57:53 -0500

    init

---
+++ bi-gram-takehome.rb
@@ -0,0 +1,28 @@
+# Sequential Bi-grams
+#    Given a string of words, create a set of every sequential bi-gram.  Bigrams are two-word combinations of the text, i.e. “Charlie rocks”.
+
+# Example
+# "Make a killer impression on whoever you’re meeting."
+
+# returns
+# “Make a"
+# “a killer"
+# “killer impression"
+# “impression on"
+# Etc.
+
+# requirements
+# Generates a set of every possible bigram from a string of text
+# Keep performance & memory usage in mind
+# Include tests
+# We prefer Ruby or Python, but you may use any language.
+
+def bi_word(input)
+  words = input.split(" ")
+  i = 0
+  words.each do |word|
+    puts word[i] + word[i + 1] unless word.nil?
+  end
+end
+
+bi_word("these are test words to test the function")
